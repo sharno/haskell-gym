94ec7d778d0446239845174b1c195e745ff265c8
Sharno <sharnoby3@gmail.com>
2018-02-26 17:47:43 -0500

    shorten code for Day1 with list of comprension

--- AdventOfCode/Day1.hs
+++ AdventOfCode/Day1.hs
@@ -1,27 +1,18 @@
 import Data.Char (digitToInt)
 
-intToList :: String -> [Int]
-intToList = map digitToInt
-
-packWithNext :: [Int] -> [(Int, Int)]
-packWithNext (x:xs) = zip (x:xs) (xs ++ [x])
-
-packWithHalf :: [Int] -> [(Int, Int)]
-packWithHalf xs = zip xs (drop midPoint xs ++ take midPoint xs)
-  where midPoint = length xs `div` 2
-
-summation :: [(Int, Int)] -> Int
-summation = sum . map (\(a, b) -> if a == b then a else 0)
+captcha :: Int -> String -> Int
+captcha n xs =
+  sum [ digitToInt x | (x, y) <- zip xs rotated, x == y ] 
+  where rotated = drop n xs ++ take n xs
 
 part1 :: String -> Int
-part1 = summation . packWithNext . intToList
+part1 = captcha 1
 
 part2 :: String -> Int
-part2 = summation . packWithHalf . intToList
+part2 xs = captcha (length xs `div` 2) xs
 
 main = do
   s <- readFile "Day1.in"
   -- let s = "1212"
-  putStrLn $ show $ intToList s
   putStrLn $ show $ part1 s
   putStrLn $ show $ part2 s
