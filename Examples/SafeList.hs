8345fb5b32a90c28a08e7eaa5194fb8157a0ea69
Sharno <sharnoby3@gmail.com>
2018-03-01 20:50:39 -0500

    GADT SafeList

---
+++ Examples/SafeList.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE GADTs #-}
+
+data Empty
+data NonEmpty
+data SafeList a b where
+  Nil :: SafeList a Empty
+  Cons :: a -> SafeList a b -> SafeList a c
+
+
+safeHead :: SafeList a NonEmpty -> a
+safeHead (Cons a _) = a
+
+
+main :: IO ()
+main = do
+  print $ safeHead (Cons 1 Nil)
+  
+  -- this gives an error:
+  -- print $ safeHead (Nil)
+  
+  return ()
